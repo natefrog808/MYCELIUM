700caa385cbc5334cd072bbe020729fa7275a4b0
natefrog808 <wetakatini@gmail.com>
2025-02-27 08:37:19 -1000

    Create init.py

---
+++ mycelium/translators/init.py
@@ -0,0 +1,5 @@
+# Import translators for easy access
+from .water_stress_translator import WaterStressTranslator
+from .biodiversity_translator import BiodiversityTranslator
+
+__all__ = ['WaterStressTranslator', 'BiodiversityTranslator']
