beddd6f09606b08fbf088bfca2308c30ad11c39d
natefrog808 <wetakatini@gmail.com>
2025-02-27 08:54:18 -1000

    Create init.py

---
+++ init.py
@@ -0,0 +1,8 @@
+"""
+MYCELIUM: The Earth's Song in Our Blood
+
+A Bioregional Consciousness Network that reestablishes human connection to place
+and restores our role as conscious participants in local ecosystems.
+"""
+
+__version__ = "0.1.0"
